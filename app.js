e993f926b381dbba6589eebca641a0e4dc1bb066
Mathieu Saubin <saubinm@free.fr>
2019-04-15 10:50:47 +0200

    Finish scrapping script + use of fs to export to result.csv

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const fs = require("fs");
+
+const writeStream = fs.createWriteStream("result.csv");
 
 const baseUrl = "https://www.efficity.com/";
 const listUrl = "consultants-immobilier/";
@@ -11,11 +14,44 @@ const customHeaderRequest = request.defaults({
   }
 });
 
+writeStream.write(`Nom,Prenom,Telephone, E-Mail \n`);
+
 customHeaderRequest.get(baseUrl + listUrl, (err, resp, body) => {
-  $ = cheerio.load(body);
-  let links = $(".c-content-list > li > a");
-  const listOfLinks = [];
-  $(links).each((i, link) => {
-    listOfLinks.push($(link).attr("href"));
-  });
+  if (!err && resp.statusCode == 200) {
+    $ = cheerio.load(body);
+    let links = $(".c-content-list > li > a");
+    const listOfLinks = [];
+    $(links).each((i, link) => {
+      listOfLinks.push($(link).attr("href"));
+    });
+    listOfLinks.forEach(link => {
+      customHeaderRequest.get(baseUrl + link, (err, resp, body) => {
+        if (!err && resp.statusCode == 200) {
+          const contact = { lastname: "", firstname: "", phone: "", email: "" };
+          $ = cheerio.load(body);
+
+          let namePosition = $(".toolbarcontact h2")
+            .text()
+            .trim()
+            .split(" ");
+          let firstname = namePosition.splice(0, 1);
+          let lastname = namePosition.join(" ");
+
+          let phonePosition = $(".toolbarcontact h3 > span");
+          let phone = $(phonePosition)
+            .text()
+            .trim()
+            .split(" ")
+            .join("");
+
+          let emailPosition = $(".toolbarcontact h3 > a");
+          let email = $(emailPosition)
+            .text()
+            .trim();
+
+          writeStream.write(`${lastname}, ${firstname}, ${phone}, ${email} \n`);
+        }
+      });
+    });
+  }
 });
