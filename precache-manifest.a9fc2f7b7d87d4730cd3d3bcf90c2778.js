73d57246899dfccedaba1e0c01cdebe182ca57d4
Mathieu Saubin <saubinm@free.fr>
2019-04-16 11:38:40 +0200

    Updates

---
+++ precache-manifest.a9fc2f7b7d87d4730cd3d3bcf90c2778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a15e0fab36b0b69bfee7",
+    "url": "/lumeeo-technical-test/static/js/runtime~main.c83ac58f.js"
+  },
+  {
+    "revision": "69ea35e08f9248e4d863",
+    "url": "/lumeeo-technical-test/static/js/main.c57da275.chunk.js"
+  },
+  {
+    "revision": "9176aa15b1850d7604fb",
+    "url": "/lumeeo-technical-test/static/js/2.ac3661bd.chunk.js"
+  },
+  {
+    "revision": "69ea35e08f9248e4d863",
+    "url": "/lumeeo-technical-test/static/css/main.34b50a27.chunk.css"
+  },
+  {
+    "revision": "9176aa15b1850d7604fb",
+    "url": "/lumeeo-technical-test/static/css/2.10c3bb53.chunk.css"
+  },
+  {
+    "revision": "057f34c16135cc46ddf2d71516064367",
+    "url": "/lumeeo-technical-test/index.html"
+  }
+];
